e76bbe1a3c72cd6c8126c2a4430fd1bbb247cf2c
Ewan Croft <git@ewancroft.uk>
2026-04-29 22:47:21 +0100

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "website-comm-template — SvelteKit community template";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      devShells = forAllSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [ nodejs_22 pnpm ];
+
+            shellHook = ''
+              echo "website-comm-template dev shell ready (Node.js 22 + pnpm)"
+            '';
+          };
+        }
+      );
+
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
+    };
+}
